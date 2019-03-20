ed6fbed01b79e0b4d5f40321c4ec5007f4fdc0f8
pbies <39916932+pbies@users.noreply.github.com>
2019-03-20 16:40:57 +0100

    Create getxchgrates.pl

---
+++ getxchgrates.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+# (C) 2019 Piotr Biesiada
+
+# Perl script to download and parse exchange rates for Polish zloty PLN to Dygraph format
+
+use IO::Socket;
+use strict;
+use warnings;
+
+use LWP::Simple;
+use XML::Simple;
+
+my $url = 'http://www.nbp.pl/kursy/xml/dir.txt';
+my $file = 'dir.txt';
+
+getstore($url, $file);
+
+open my $handle, '<', $file;
+chomp(my @lines = <$handle>);
+close $handle;
+
+my @files;
+
+foreach (@lines)
+{
+	if (ord($_)==97)
+	{
+		chomp($_);
+		push @files, $_;
+	}
+}
+
+open(my $outh, '>', 'output.txt');
+
+print $outh "D,EUR,USD,CHF,GBP\n";
+
+for my $i (0 .. $#files)
+{
+	my $fn = $files[$i];
+	chop $fn;
+	$fn = $fn.'.xml';
+	my $url = 'http://www.nbp.pl/kursy/xml/'.$fn;
+
+	getstore($url, $fn);
+
+	my $xml = XMLin($fn);
+	print $outh $xml->{data_publikacji}.',';
+	print $outh $xml->{pozycja}[7]{kurs_sredni} =~ s/,/./r .',';
+	print $outh $xml->{pozycja}[7]{kurs_sredni} =~ s/,/./r .',';
+	print $outh $xml->{pozycja}[7]{kurs_sredni} =~ s/,/./r .',';
+	print $outh $xml->{pozycja}[7]{kurs_sredni} =~ s/,/./r ."\n";
+
+}
