40ad6ef0b01da358b4f2f179038d2d973f7b8031
pbies <39916932+pbies@users.noreply.github.com>
2019-06-13 02:13:11 +0200

    Update same_content.pl

--- same_content.pl
+++ same_content.pl
@@ -4,29 +4,38 @@ use strict;
 use warnings;
 
 my @files = <*.txt>;
+my $count = @files;
 my @del;
 my $diff;
-foreach my $file1 (@files) {
-	foreach my $file2 (@files) {
+my $i = 0;
+my $file1;
+my $file2;
+my $fh1;
+my $fh2;
+my $line1;
+my $line2;
+
+foreach $file1 (@files) {
+	foreach $file2 (@files) {
 		if ($file1 eq $file2) {
 			last;
 		}
-		open my $fh1, $file1 or die "can't open $file1: $!";
-		open my $fh2, $file2 or die "can't open $file2: $!";
-		print "Comparing $file1 and $file2...";
-		my $line1;
-		my $line2;
+		open $fh1, $file1 or die "can't open $file1: $!";
+		open $fh2, $file2 or die "can't open $file2: $!";
+#		print "Comparing $file1 and $file2...";
 		$diff=0;
 		while($line1 = <$fh1>) {
+			if (!$line1) { $line1 = ""; }
 			$line2 = <$fh2>;
-			if ($line1 ne $line2) {
-				print "different!\n";
+			if (!$line2) { $line2 = ""; }
+			if ("$line1" ne "$line2") {
+#				print "different!\n";
 				$diff=1;
 				last;
 			}
 		}
 		if ($diff == 0) {
-			print "the same till end of one files!\n";
+#			print "the same till end of one files!\n";
 			if (-s $file1 >= -s $file2)
 			{
 				push @del, $file2;
@@ -39,7 +48,12 @@ foreach my $file1 (@files) {
 		close($fh1);
 		close($fh2);
 	}
+	$i++;
+	print "\r$i/$count";
 }
+
+print "\n";
+
 foreach my $file (@del) {
 	print "Removing $file\n";
 	unlink $file;
