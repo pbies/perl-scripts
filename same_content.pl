f1d5f5e9244bc41a3cfbfe88902a556152b19187
pbies <39916932+pbies@users.noreply.github.com>
2019-06-24 08:20:23 +0200

    Update same_content.pl

--- same_content.pl
+++ same_content.pl
@@ -18,14 +18,14 @@ my $fh2;
 my $line1;
 my $line2;
 
-foreach $file1 (@files) {
+while($file1=shift(@files)) {
 	SAME:
 	foreach $file2 (@files) {
 		if ($file1 eq $file2) {
 			next;
 		}
-		open $fh1, $file1 or die "can't open $file1: $!";
-		open $fh2, $file2 or die "can't open $file2: $!";
+		open $fh1, $file1 or die "can't open $file1: $!\n";
+		open $fh2, $file2 or die "can't open $file2: $!\n";
 #		print "Comparing $file1 and $file2...\n";
 		$diff=0;
 		while($line1 = <$fh1>) {
@@ -43,25 +43,25 @@ foreach $file1 (@files) {
 			if (-s $file1 >= -s $file2)
 			{
 				push @del, $file2;
-				print "\n$file2\n";
+				print "\n$file1 >= $file2\n";
 			}
 			if (-s $file1 < -s $file2)
 			{
 				push @del, $file1;
-				print "\n$file1\n";
+				print "\n$file2 >= $file1\n";
 			}
 		}
 	}
+	close($fh1);
+	close($fh2);
 	$i++;
 	print "\r$i/$count";
 }
 
-close($fh1);
-close($fh2);
 
 print "\n";
 
 foreach my $file (@del) {
-	print "Removing $file\n";
 	unlink $file;
+	print "Removed $file\n";
 }
