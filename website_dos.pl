1a5e96003e8c53ea2f3c6b94fee6750c78f8f08f
pbies <39916932+pbies@users.noreply.github.com>
2019-07-04 14:04:29 +0200

    Update website_dos.pl

--- website_dos.pl
+++ website_dos.pl
@@ -6,12 +6,14 @@
 use IO::Socket;
 use strict;
 
+my $addr = "yourwebsite.com";
+
 for ( 1 .. 128 ) {
     my ($response);
     my ($SOCKET) =
-      new IO::Socket::INET( Proto => "tcp", PeerAddr => "website.com:80" );
+      new IO::Socket::INET( Proto => "tcp", PeerAddr => "$addr:80" );
     if ( !defined $SOCKET ) { die $!; }
-    print $SOCKET "GET /?$_ HTTP/1.1\r\nHost: website.com\r\n\r\n";
+    print $SOCKET "GET /?$_ HTTP/1.1\r\nHost: $addr\r\n\r\n";
     $response = <$SOCKET>;
     print "$_ $response";
     close($SOCKET);
